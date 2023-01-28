efb6f4710d3844d4c361e21f895ed3c4bf67c3e4
aleksandr <aleksandrklysa7@gmail.com>
2023-01-28 13:06:59 +0200

    dz2_1 commit

---
+++ Project2/dz2_1.py
@@ -0,0 +1,11 @@
+a = int(input('Введите четырехзначное число:'))
+b = 10000
+x = 1000
+y = 100
+z = 10
+left, right = divmod(a, b)
+n, m = divmod(right, x)
+n1, m1 = divmod(m, y)
+n2, m2 = divmod(m1, z)
+left, n, n1, n2, m2 = m2, n2, n1, n, left
+print(left, n, n1, n2, m2)
