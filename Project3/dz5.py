c3ce38787af937eeabb97cad4fc2dea75e1cacd0
aleksandr <aleksandrklysa7@gmail.com>
2023-02-02 01:07:24 +0200

    second commit

---
+++ Project3/dz5.py
@@ -0,0 +1,15 @@
+lst = [1, 2, 3, 4, 5, 6]
+lst1 = lst[:3]
+lst2 = lst[3:]
+
+# lst = [1, 2, 3]
+# lst1 = lst[:2]
+# lst2 = lst[2:]
+
+# lst = []
+# lst1 = lst[:1]
+# lst2 = lst[1:]
+
+x = [lst1, lst2]
+print(x)
+
