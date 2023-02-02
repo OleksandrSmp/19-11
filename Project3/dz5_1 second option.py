fe42b1f3d8746b0d2e77be33ca4e86c252a846db
aleksandr <aleksandrklysa7@gmail.com>
2023-02-02 02:08:42 +0200

    second commit

--- Project3/dz5_1 second option.py
+++ Project3/dz5_1 second option.py
@@ -1,18 +1,16 @@
 lst = [1, 2, 3, 4, 5, 6]
+# lst = [1, 2, 3]
+# lst = []
+
 lst1 = lst[:3]
 lst2 = lst[3:]
 
-# lst = [1, 2, 3]
-# lst1 = lst[:2]
-# lst2 = lst[2:]
+lst3 = lst[:2]
+lst4 = lst[2:]
 
-# lst = []
-# lst1 = lst[:1]
-# lst2 = lst[1:]
-
-if len(lst) == 3:
+if len(lst) == 6:
     print([lst1, lst2])
-elif len(lst) == 6:
-    print([lst1, lst2])
+elif len(lst) == 3:
+    print([lst3, lst4])
 elif len(lst) == 0:
-    print([lst1, lst2])
+    print([lst3, lst4])
