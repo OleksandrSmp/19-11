a0758dbd995e5228424663ddadad892aaabf3115
aleksandr <aleksandrklysa7@gmail.com>
2023-02-05 00:55:29 +0200

    second commit

--- Project3/dz_5 retransmission.py
+++ Project3/dz_5 retransmission.py
@@ -4,15 +4,15 @@
 # a = (lst[:lst2])
 # b = (lst[lst2:])
 
-lst = [1, 2, 3]
-lst1 = len(lst)
-a = (lst[:lst1][:-1])
-b = (lst[:lst1][-1:])
-
-# lst = []
+# lst = [1, 2, 3]
 # lst1 = len(lst)
-# a = (lst[:lst1])
-# b = (lst[lst1:])
+# a = (lst[:lst1][:-1])
+# b = (lst[:lst1][-1:])
+
+lst = []
+lst1 = len(lst)
+a = (lst[:lst1])
+b = (lst[lst1:])
 
 print([a, b])
 
