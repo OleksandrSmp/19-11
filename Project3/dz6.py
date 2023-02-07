b54c3ada38c8176efb50923413c44be8c62584e2
aleksandr <aleksandrklysa7@gmail.com>
2023-02-07 02:14:54 +0200

    second commit

---
+++ Project3/dz6.py
@@ -0,0 +1,9 @@
+# lst = [12, 3, 4, 10]
+# lst = [1]
+lst = []
+if len(lst) != 0:
+    x = lst.pop()
+    lst.insert(0, x)
+    print(lst)
+else:
+    print(lst[:])
---
+++ Project3/dz6_1.py
@@ -0,0 +1,5 @@
+lst = [12, 3, 4, 10]
+# lst = [1]
+# lst = []
+lst1 = lst[-1:] + lst[:-1]
+print(lst1)
