7aee18b6dac5d8abe0eb54227b8a5f26beda2592
aleksandr <aleksandrklysa7@gmail.com>
2023-02-14 00:57:23 +0200

    second commit

--- Project4/dz9.py
+++ Project4/dz9.py
@@ -1,8 +1,9 @@
-lst = [1, 2, 3, 4, 5, 6, 7, 9]
+# lst = [1, 2, 3, 4, 5, 6, 7, 9]
 # lst = [1, 1, 1, 1]
 # lst = [6, 3, 7]
 import random
-lst1= [random.randint(1, 100) for i in range(random.randint(3, 10))]
-lst = ([lst[0]] + [lst[2]] + [lst[-2]])
+lst1 = [random.randint(1, 100) for i in range(random.randint(3, 10))]
+print(lst1)
+lst = ([lst1[0]] + [lst1[2]] + [lst1[-2]])
 print(lst)
 
