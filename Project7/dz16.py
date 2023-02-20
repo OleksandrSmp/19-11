4c6265c00549808bd4df8eb33e1a76a1e937b12c
aleksandr <aleksandrklysa7@gmail.com>
2023-02-20 04:15:23 +0200

    second commit

--- Project7/dz16.py
+++ Project7/dz16.py
@@ -1,6 +1,6 @@
 def say_hi(name: str, age: int):
     return "Привет, меня зовут {} и мне {} лет.".format(name, age)
-name = "Иван"
-age = 30
+name = "Александр"
+age = 35
 greeting = say_hi(name, age)
 print(greeting)
