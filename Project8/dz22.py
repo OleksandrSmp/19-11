736513474292540db4f10df96e67ffacfaf04c66
aleksandr <aleksandrklysa7@gmail.com>
2023-02-23 12:52:54 +0200

    second commit

---
+++ Project8/dz22.py
@@ -0,0 +1,8 @@
+def find_unique_value(some_list):
+    for char in some_list:
+        if some_list.count(char) == 1:
+            return char
+assert find_unique_value([1, 2, 1, 1]) == 2, 'Test1'
+assert find_unique_value([2, 3, 3, 3, 5, 5]) == 2, 'Test2'
+assert find_unique_value([5, 5, 5, 2, 2, 0.5]) == 0.5, 'Test3'
+print("ОК")
