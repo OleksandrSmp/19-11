4c41f50a40208c2f40728414bf08ad9016a58719
aleksandr <aleksandrklysa7@gmail.com>
2023-02-23 12:30:17 +0200

    second commit

--- Project8/dz21.py
+++ Project8/dz21.py
@@ -3,8 +3,8 @@ def is_palindrome(text):
     for char in text:
         if char.isalnum():
             clean_text += char.lower()
-            return clean_text == clean_text[::-1]
-            assert is_palindrome('A man, a plan, a canal: Panama') == True, 'Test1'
-            assert is_palindrome('0P') == False, 'Test2'
-            assert is_palindrome('a.') == True, 'Test3'
+    return clean_text == clean_text[::-1]
+assert is_palindrome('A man, a plan, a canal: Panama') == True, 'Test1'
+assert is_palindrome('0P') == False, 'Test2'
+assert is_palindrome('a.') == True, 'Test3'
 print("ОК")
