1caf233dbf4c2398701f3ecb9212149ee050c467
aleksandr <aleksandrklysa7@gmail.com>
2023-02-24 17:51:44 +0200

    second commit

---
+++ Project9/dz24.py
@@ -0,0 +1,10 @@
+def difference(*args):
+    if len(args) == 0:
+        return 0
+    else:
+        return max(args) - min(args)
+    assert difference(1, 2, 3) == 2, 'Test1'
+    assert difference(5, -5) == 10, 'Test2'
+    assert difference(10.2, -2.2, 0, 1.1, 0.5) == 12.4, 'Test3'
+    assert difference() == 0, 'Test4'
+print('OK')
