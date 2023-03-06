11ee6f067fb9bbe3e64fe617115003f5a51125e7
aleksandr <aleksandrklysa7@gmail.com>
2023-03-06 05:17:41 +0200

    second commit

---
+++ Project11/dz30.py
@@ -0,0 +1,5 @@
+def is_even(number):
+    return (number & 1) == 0
+assert is_even(2494563894038**2) == True, 'Test1'
+assert is_even(1056897**2) == False, 'Test2'
+assert is_even(24945638940387**3) == False, 'Test3'
