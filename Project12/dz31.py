ba03e5c3368c0669d84c4eedec733f9d0f9d88e5
aleksandr <aleksandrklysa7@gmail.com>
2023-03-08 18:21:45 +0200

    second commit

---
+++ Project12/dz31.py
@@ -0,0 +1,14 @@
+import codecs
+import re
+def delete_html_tags(html_file, result_file = 'cleaned.txt'):
+    with codecs.open(html_file, 'r', 'utf-8') as file:
+        html = file.read()
+        cleaned_text = re.sub('<.*?>', '', html)
+        lines = filter(lambda x: x.strip(), cleaned_text.splitlines())
+        cleaned_text = '\n'.join(lines)
+    with codecs.open(result_file, 'w', 'utf-8') as file:
+        file.write(cleaned_text)
+
+
+
+
