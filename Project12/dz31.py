557bc60004a9eba01728079b6c8805bf431ef467
aleksandr <aleksandrklysa7@gmail.com>
2023-03-09 13:12:48 +0200

    second commit

--- Project12/dz31.py
+++ Project12/dz31.py
@@ -4,10 +4,10 @@ def delete_html_tags(html_file, result_file='cleaned.txt'):
     with open(html_file, 'r', encoding='utf-8') as file:
         html = file.read()
         cleaned_text = re.sub('<.*?>', '', html)
-    with open(result_file, 'w', encoding='utf-8') as file:
+    with open(result_file, 'w', encoding = 'utf-8') as file:
         file.write(cleaned_text)
         delete_html_tags('example.html', 'cleaned.txt')
-    with open('cleaned.txt', 'r', encoding='utf-8') as file:
+    with open('cleaned.txt', 'r', encoding = 'utf-8') as file:
         cleaned_text = file.read()
     print(cleaned_text)
 
