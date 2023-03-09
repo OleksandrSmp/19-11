81335bcb923248524063af68a2027198f3b7cae0
aleksandr <aleksandrklysa7@gmail.com>
2023-03-09 17:33:08 +0200

    second commit

--- Project12/dz31.py
+++ Project12/dz31.py
@@ -1,12 +1,13 @@
-from bs4 import BeautifulSoup
 import codecs
-def delete_html_tags(html_file, result_file='cleaned.txt'):
-    with codecs.open(html_file, 'r', encoding='utf-8') as file:
+import re
+
+def delete_html_tags(html_file, result_file = 'cleaned.txt'):
+    with open(html_file, 'r', encoding = 'utf-8') as file:
         html = file.read()
-    num = BeautifulSoup(html, 'html.parser')
-    cleaned_text = num.get_text()
-    with codecs.open(result_file, 'w', encoding='utf-8') as file:
+        cleaned_text = re.sub('<.*?>', '', html)
+    with open(result_file, 'w', encoding = 'utf-8') as file:
         file.write(cleaned_text)
+
 delete_html_tags('draft.html', 'cleaned.txt')
 
 
