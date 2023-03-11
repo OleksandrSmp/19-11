a83be577965790fa5b6fc344b5109a2b0a03c1a5
aleksandr <aleksandrklysa7@gmail.com>
2023-03-11 11:34:08 +0200

    second commit

---
+++ Project13/dz33.py
@@ -0,0 +1,42 @@
+class Human:
+    def __init__(self, gender, age, first_name, last_name):
+        self.gender = gender
+        self.age = age
+        self.first_name = first_name
+        self.last_name = last_name
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}, {self.age}, {self.gender}'
+class Student(Human):
+    def __init__(self, gender, age, first_name, last_name, record_book):
+        super().__init__(gender, age, first_name, last_name)
+        self.record_book = record_book
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}, {self.age}, {self.gender}, {self.record_book}'
+class Group:
+    def __init__(self, number):
+        self.number = number
+        self.group = set()
+    def add_student(self, student):
+        self.group.add(student)
+    def delete_student(self, last_name):
+        student = self.find_student(last_name)
+        if student is not None:
+            self.group.remove(student)
+    def find_student(self, last_name):
+        for student in self.group:
+            if student.last_name == last_name:
+                return student
+        return None
+    def __str__(self):
+        all_students = '\n'.join([str(student) for student in self.group])
+        return f'Number:{self.number}\n{all_students}'
+st1 = Student('Male', 30, 'Steve', 'Jobs', 'AN142')
+st2 = Student('Female', 25, 'Liza', 'Taylor', 'AN145')
+gr = Group('PD1')
+gr.add_student(st1)
+gr.add_student(st2)
+print(gr)
+assert str(gr.find_student('Jobs')) == str(st1), 'Test1'
+assert gr.find_student('Jobs2') is None, 'Test2'
+gr.delete_student('Taylor')
+print(gr)
