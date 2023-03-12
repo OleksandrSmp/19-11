eabbe7346df9fac67aeed8f6cb30fa8fd48aa629
aleksandr <aleksandrklysa7@gmail.com>
2023-03-12 15:40:29 +0200

    second commit

--- Project13/dz33.py
+++ Project13/dz33.py
@@ -4,6 +4,7 @@ class Human:
         self.age = age
         self.first_name = first_name
         self.last_name = last_name
+
     def __str__(self):
         return f'{self.first_name} {self.last_name}, {self.age}, {self.gender}'
 
@@ -11,6 +12,7 @@ class Student(Human):
     def __init__(self, gender, age, first_name, last_name, record_book):
         super().__init__(gender, age, first_name, last_name)
         self.record_book = record_book
+
     def __str__(self):
         return f'{self.first_name} {self.last_name}, {self.age}, {self.gender}, {self.record_book}'
 
@@ -18,17 +20,21 @@ class Group:
     def __init__(self, number):
         self.number = number
         self.group = set()
+
     def add_student(self, student):
         self.group.add(student)
+
     def delete_student(self, last_name):
         student = self.find_student(last_name)
         if student is not None:
             self.group.remove(student)
+
     def find_student(self, last_name):
         for student in self.group:
             if student.last_name == last_name:
                 return student
         return None
+
     def __str__(self):
         all_students = '\n'.join([str(student) for student in self.group])
         return f'Number:{self.number}\n{all_students}'
