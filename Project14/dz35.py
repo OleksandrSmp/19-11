81a95a4ee9bd27f8d99075f89ccdc9cb56f6cc32
aleksandr <aleksandrklysa7@gmail.com>
2023-03-12 19:12:05 +0200

    second commit

---
+++ Project14/dz35.py
@@ -0,0 +1,64 @@
+class Human:
+
+    def __init__(self, gender, age, first_name, last_name):
+        self.gender = gender
+        self.age = age
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}, {self.age}, {self.gender}'
+
+class Student(Human):
+
+    def __init__(self, gender, age, first_name, last_name, record_book):
+        super().__init__(gender, age, first_name, last_name)
+        self.record_book = record_book
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}, {self.age}, {self.gender}, {self.record_book}'
+
+class Group:
+    class GroupOverflowError(Exception):
+        pass
+
+    MAX_STUDENTS = 10
+
+    def __init__(self, number):
+        self.number = number
+        self.group = set()
+
+    def add_student(self, student):
+        if len(self.group) >= self.MAX_STUDENTS:
+            raise self.GroupOverflowError(f"Group {self.number} is full (max {self.MAX_STUDENTS} students)")
+        self.group.add(student)
+
+    def delete_student(self, last_name):
+        student = self.find_student(last_name)
+        if student is not None:
+            self.group.remove(student)
+
+    def find_student(self, last_name):
+        for student in self.group:
+            if student.last_name == last_name:
+                return student
+        return None
+
+    def __str__(self):
+        all_students = '\n'.join([str(student) for student in self.group])
+        return f'Number:{self.number}\n{all_students}'
+
+st1 = Student('Male', 30, 'Steve', 'Jobs', 'AN142')
+st2 = Student('Female', 25, 'Liza', 'Taylor', 'AN145')
+gr = Group('PD1')
+gr.add_student(st1)
+gr.add_student(st2)
+
+for i in range(10):
+    try:
+        st = Student('Male', 20+i, f'Name{i}', f'Surname{i}', f'AN{i}')
+        gr.add_student(st)
+    except Group.GroupOverflowError as e:
+        print(f"Error: {e}")
+        break
+print(gr)
