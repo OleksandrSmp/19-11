04f2c85b6f8fa8d1879ced9107c3ad6ac0595e3c
aleksandr <aleksandrklysa7@gmail.com>
2023-03-12 15:42:13 +0200

    second commit

--- Project12/dz32.py
+++ Project12/dz32.py
@@ -4,29 +4,37 @@ class Item:
         self.price = price
         self.description = description
         self.dimensions = dimensions
+
     def __str__(self):
         return f'{self.name}, price: {self.price}'
+
 class User:
     def __init__(self, name, surname, numberphone):
         self.name = name
         self.surname = surname
         self.numberphone = numberphone
+
     def __str__(self):
         return f'{self.name} {self.surname}'
+
 class Purchase:
     def __init__(self, user):
         self.products = {}
         self.user = user
         self.total = 0
+
     def add_item(self, item, cnt):
         self.products[item] = cnt
+
     def __str__(self):
         items_str = '\n'.join([f'{item.name}: {self.products[item]} pcs.' for item in self.products])
         return f'User: {self.user}\nItems:\n{items_str}'
+
     def get_total(self):
         for item in self.products:
             self.total += item.price * self.products[item]
         return self.total
+
 lemon = Item('lemon', 5, "yellow", "small")
 apple = Item('apple', 2, "red", "middle")
 print(lemon)  # lemon, price: 5
