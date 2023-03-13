96eeefe8869dad779f7b6bc9eefce8985b2bcb70
aleksandr <aleksandrklysa7@gmail.com>
2023-03-13 23:52:56 +0200

    second commit

--- Project14/dz36/student.py
+++ Project14/dz36/student.py
@@ -2,12 +2,15 @@ from Project14.dz36.human import Human
 
 class Student(Human):
 
-    def __eq__(self, other):
-        if not isinstance(other, Student):
-            return False
-        return (self.gender == other.gender
-                and self.age == other.age
-                and self.first_name == other.first_name
-                and self.last_name == other.last_name
-                and self.record_book == other.record_book)
+    def __init__(self, gender, age, first_name, last_name, record_book):
+        super().__init__(gender, age, first_name, last_name)
+        self.record_book = record_book
 
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}, {self.age}, {self.gender}, {self.record_book}'
+
+    def __hash__(self):
+        return hash(self.record_book)
+
+    def __eq__(self, other):
+        return isinstance(other, Student) and self.record_book == other.record_book
