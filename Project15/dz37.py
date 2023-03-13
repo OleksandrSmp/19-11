627d39ba7b9dbc43ed6e23b6d8425688469317d6
aleksandr <aleksandrklysa7@gmail.com>
2023-03-13 23:55:16 +0200

    second commit

---
+++ Project15/dz37.py
@@ -0,0 +1,37 @@
+class Rectangle:
+
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def get_square(self):
+        return self.width * self.height
+
+    def __eq__(self, other):
+        return self.get_square() == other.get_square()
+
+    def __add__(self, other):
+        new_width = self.width + other.width
+        new_height = self.height + other.height
+        return Rectangle(new_width, new_height)
+
+    def __mul__(self, n):
+        new_width = self.width * n
+        new_height = self.height * n
+        return Rectangle(new_width, new_height)
+
+    def __str__(self):
+        return f'Rectangle: width={self.width}, height={self.height}'
+
+
+r1 = Rectangle(2, 4)
+r2 = Rectangle(3, 6)
+assert r1.get_square() == 8
+assert r2.get_square() == 18
+
+r3 = r1 + r2
+assert r3.get_square() == 54
+
+r4 = r1 * 4
+assert r4.get_square() == 32
+print("All tests passed!")
--- main.py
+++ main.py
@@ -1,12 +1,60 @@
-seconds = int(input("Введите число секунд: "))
+class Item:
 
-days = seconds // (24 * 3600)
-seconds = seconds % (24 * 3600)
+    def __init__(self, name, price, description, dimensions):
+        self.price = price
+        self.description = description
+        self.dimensions = dimensions
+        self.name = name
 
-hours = seconds // 3600
-seconds = seconds % 3600
+    def __str__(self):
+        return f"{self.name}, price: {self.price}"
 
-minutes = seconds // 60
-seconds = seconds % 60
+class User:
 
-print("Время: {:02d}:{:02d}:{:02d}:{:02d}".format(days, hours, minutes, seconds))
+    def __init__(self, name, surname, numberphone):
+        self.name = name
+        self.surname = surname
+        self.numberphone = numberphone
+
+    def __str__(self):
+        return f"{self.name} {self.surname}"
+
+class Purchase:
+    def __init__(self, user):
+        self.products = {}
+        self.user = user
+        self.total = 0
+
+    def add_item(self, item, cnt):
+        self.products[item] = cnt
+
+    def __str__(self):
+        res = f"User: {self.user}\nItems:\n"
+        for item, cnt in self.products.items():
+            res += f"{item.name}: {cnt} pcs.\n"
+        return res
+
+    def get_total(self):
+        for item, cnt in self.products.items():
+            self.total += item.price * cnt
+        return self.total
+
+
+lemon = Item('lemon', 5, "yellow", "small", )
+apple = Item('apple', 2, "red", "middle", )
+print(lemon)  # lemon, price: 5
+
+buyer = User("Ivan", "Ivanov", "02628162")
+print(buyer)  # Ivan Ivanov
+
+cart = Purchase(buyer)
+cart.add_item(lemon, 4)
+cart.add_item(apple, 20)
+print(cart)
+"""
+User: Ivan Ivanov
+Items:
+lemon: 4 pcs.
+apple: 20 pcs.
+"""
+print(cart.get_total()) # 45 + 40 = 85
