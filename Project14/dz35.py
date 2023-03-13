610cee621f8156165cc861da4f45a62b8cf818ff
aleksandr <aleksandrklysa7@gmail.com>
2023-03-13 19:09:22 +0200

    second commit

--- Project14/dz35.py
+++ Project14/dz35.py
@@ -18,9 +18,11 @@ class Student(Human):
     def __str__(self):
         return f'{self.first_name} {self.last_name}, {self.age}, {self.gender}, {self.record_book}'
 
+class GroupOverflowError(Exception):
+    pass
+
 class Group:
-    class GroupOverflowError(Exception):
-       pass
+
     MAX_STUDENTS = 10
 
     def __init__(self, number):
@@ -29,23 +31,8 @@ class Group:
 
     def add_student(self, student):
         if len(self.group) >= self.MAX_STUDENTS:
-            raise self.GroupOverflowError(f"Group {self.number} is full (max {self.MAX_STUDENTS} students)")
+            raise GroupOverflowError(f"Group {self.number} is full (max {self.MAX_STUDENTS} students)")
         self.group.add(student)
-
-    def delete_student(self, last_name):
-        student = self.find_student(last_name)
-        if student is not None:
-            self.group.remove(student)
-
-    def find_student(self, last_name):
-        for student in self.group:
-            if student.last_name == last_name:
-                return student
-        return None
-
-    def __str__(self):
-        all_students = '\n'.join([str(student) for student in self.group])
-        return f'Number:{self.number}\n{all_students}'
 
 st1 = Student('Male', 30, 'Steve', 'Jobs', 'AN142')
 st2 = Student('Female', 25, 'Liza', 'Taylor', 'AN145')
@@ -57,7 +44,7 @@ for i in range(10):
     try:
         st = Student('Male', 20 + i, f'Name{i}', f'Surname{i}', f'AN{i}')
         gr.add_student(st)
-    except Group.GroupOverflowError as e:
+    except GroupOverflowError as e:
         print(f"Error: {e}")
         break
 print(gr)
